86ec16d459bc9a4b76bbe04af70df925e0b34cc6
lmuniv00 <lmuniv00@fesb.hr>
2023-03-16 15:50:52 +0100

    promjena

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Luka_Munivrana
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Novo");
         }
     }
 }
